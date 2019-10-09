7f759111862691164bc5f988a2e6d76f0c6ac18f
artsiom_tsaryonau <artemii1992@gmail.com>
2019-10-09 22:36:08 +0200

    Initial commit

---
+++ src/test/java/com/github/spring/docker/samplespringdocker/SampleSpringDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.spring.docker.samplespringdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSpringDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
